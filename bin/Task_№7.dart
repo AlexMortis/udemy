7374129325fb4acabe5cebf548607d353303928b
AlexMortis <semen4a@gmail.com>
2023-10-23 13:21:06 +0300

    fix

--- bin/Task_№7.dart
+++ bin/Task_№7.dart
@@ -4,27 +4,25 @@
 // 2. Если в списке все отрицательные значения, то вернуть 0.
 
 void main () {
-  var wordsList = [-1,1,-3,5,-10,20];
-  numbList(wordsList);
-}
-
-List numbList(List listOfString) {
-  var sumOfList = [];
-  var numb = 0;
-  if (listOfString.isEmpty == true){
-    print(0);
-  }
-  else{
-    listOfString.removeWhere((element) => element < 0);
-    if(listOfString.isEmpty == true) {
+  numbList(List listOfString) {
+    var sumOfList = [];
+    var numb = 0;
+    if (listOfString.isEmpty == true) {
       print(0);
-    }else{
-      for(var i = 0; i < listOfString.length; i++){
-        sumOfList.add(listOfString[numb]);
-        numb++;
+    }
+    else {
+      listOfString.removeWhere((element) => element < 0);
+      if (listOfString.isEmpty == true) {
+        print(0);
+      } else {
+        for (var i = 0; i < listOfString.length; i++) {
+          sumOfList.add(listOfString[numb]);
+          numb++;
+        }
+        print(sumOfList.reduce((a, b) => a + b));
       }
-      print(sumOfList.reduce((a, b) => a+b));
     }
   }
-  return sumOfList;
+  var wordsList = [-1,1,-3,5,-10,20];
+  numbList(wordsList);
 }

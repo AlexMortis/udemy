4c7d274c8e4cd6434b44aba74cadf7884d270c00
AlexMortis <semen4a@gmail.com>
2023-10-30 17:55:45 +0300

    fix

--- bin/Task_№5.dart
+++ bin/Task_№5.dart
@@ -5,15 +5,37 @@
 // Вам будет предоставлен список строк, и ваша задача будет вернуть значения строк,
 // умноженные на позицию этой строки в списке. Позиция начинается с 1.
 
+// void main (){
+//   print(listOfNumb(["dart", "abc", "good luck"]));
+// }
+//
+// int sumOfString (String words){
+//   var alphabet = "abcdefghijklmnopqrstuvwxyz";
+//   var sum = 0;
+//   for (var i = 0; i < words.length; i++) {
+//     sum += alphabet.indexOf(words[i]) + 1;
+//   }
+//   return sum;
+// }
+//
+// List listOfNumb (List listOfStrings){
+//   List emptyList = [];
+//   int value = 1;
+//   for(var word in listOfStrings){
+//     emptyList.add(sumOfString(word) * value);
+//     value++;
+//   }
+//   return emptyList;
+// }
+
 void main (){
   print(listOfNumb(["dart", "abc", "good luck"]));
 }
 
 int sumOfString (String words){
-  var alphabet = "abcdefghijklmnopqrstuvwxyz";
   var sum = 0;
   for (var i = 0; i < words.length; i++) {
-    sum += alphabet.indexOf(words[i]) + 1;
+    sum = words.codeUnits[i] == 32 ? (sum += words.codeUnits[i] - 32) : (sum += words.codeUnits[i] - 96);
   }
   return sum;
 }
